2a33a8fa2ad9936e2115615e54dd630d95d07a82
Krisnamahda <142768578+Krisnamahda@users.noreply.github.com>
2024-06-19 09:52:11 +0700

    Krisna_517799_Pertanyaan 3.py

--- Kelas A2/Pertemuan 1/Krisna Budhiantoro-Prak 1/Krisna_517799_Pertanyaan 3.py
+++ Kelas A2/Pertemuan 1/Krisna Budhiantoro-Prak 1/Krisna_517799_Pertanyaan 3.py
@@ -1,15 +1,38 @@
-bilangan = int(input("Masukkan angka: "))
-
-if bilangan <= 1:
-    print(f"{bilangan} bukan bilangan prima.")
-else:
-    prima = True
-    for i in range(2, int(bilangan ** 0.5) + 1):
-        if bilangan % i == 0:
-            prima = False
-            break
-
-    if prima:
-        print(f"{bilangan} adalah bilangan prima.")
+def hitung_nilai_huruf(nilai_akhir):
+    if nilai_akhir >= 80:
+        return 'A'
+    elif nilai_akhir >= 70:
+        return 'B'
+    elif nilai_akhir >= 60:
+        return 'C'
+    elif nilai_akhir >= 50:
+        return 'D'
     else:
-        print(f"{bilangan} bukan bilangan prima.")
+        return 'E'
+
+while True:
+    while True:
+        niu = input("Masukkan NIU (6 digit integer): ")
+        if len(niu) == 6 and niu.isdigit():
+            break
+        else:
+            print("NIU harus berupa 6 digit integer. Silakan coba lagi.")
+    
+    nilai_tugas = float(input("Masukkan nilai tugas: "))
+    nilai_laporan = float(input("Masukkan nilai laporan: "))
+    
+    rata_rata = (nilai_tugas + nilai_laporan) / 2
+    
+    if rata_rata < 40:
+        print("Rata-rata nilai tugas dan laporan kurang dari 40. Siswa mendapat nilai K.")
+    else:
+        nilai_ujian = float(input("Masukkan nilai ujian: "))
+        
+        nilai_akhir = 0.25 * rata_rata + 0.5 * nilai_ujian
+        
+        nilai_huruf = hitung_nilai_huruf(nilai_akhir)
+        print(f"Rata-rata nilai tugas dan laporan: {rata_rata:.2f}")
+        print(f"Nilai akhir dengan bobot tugas (25%), laporan (25%), dan ujian (50%): {nilai_akhir:.2f}")
+        print(f"Nilai akhir huruf: {nilai_huruf}")
+    
+    break
