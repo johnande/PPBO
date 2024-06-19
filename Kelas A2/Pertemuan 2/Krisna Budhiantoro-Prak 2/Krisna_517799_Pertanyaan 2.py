32be0f88d3535d116049f3483f906374adddc499
Krisnamahda <142768578+Krisnamahda@users.noreply.github.com>
2024-06-19 12:06:08 +0700

    Krisna_517799_Pertanyaan 2.py

--- Kelas A2/Pertemuan 2/Krisna Budhiantoro-Prak 2/Krisna_517799_Pertanyaan 2.py
+++ Kelas A2/Pertemuan 2/Krisna Budhiantoro-Prak 2/Krisna_517799_Pertanyaan 2.py
@@ -1,6 +1,24 @@
-class PySolution:
-    def reverse_words(self, s):
-        return ' '.join(reversed(s.split()))
+class Persegi:
+    def __init__(self, sisi):
+        self.sisi = sisi
 
-x = input("Masukkan Nama: ")
-print(PySolution().reverse_words(x))
+    def hitung_luas(self):
+        luas = self.sisi ** 2
+        return luas
+
+    def hitung_keliling(self):
+        keliling = 4 * self.sisi
+        return keliling
+
+def main():
+    sisi_persegi = float(input("Masukkan panjang sisi persegi: "))
+    persegi = Persegi(sisi_persegi)
+
+    luas_persegi = persegi.hitung_luas()
+    keliling_persegi = persegi.hitung_keliling()
+
+    print(f"Luas persegi: {luas_persegi}")
+    print(f"Keliling persegi: {keliling_persegi}")
+
+if __name__ == "__main__":
+    main()
