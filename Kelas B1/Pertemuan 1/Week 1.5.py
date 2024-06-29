e2dd8f2d9f34a6d4057352a52fdb614e1d74c1c1
Fatihibnukhovi <143493111+Fatihibnukhovi@users.noreply.github.com>
2024-06-29 15:58:48 +0700

    Delete Kelas B1/Pertemuan 1/Week 1.5.py

--- Kelas B1/Pertemuan 1/Week 1.5.py
+++
@@ -1,7 +0,0 @@
-jari_jari = float(input("masukan jari-jari (cm): "))
-
-keliling = 2 * 3.14 * jari_jari
-luas = 3.14 * jari_jari**2
-
-print("Keliling Lingkaran:", keliling, "cm")
-print("Luas Lingkaran:", luas, "cm")
